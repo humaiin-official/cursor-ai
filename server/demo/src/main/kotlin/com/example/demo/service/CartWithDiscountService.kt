0cd9e79c79d8f285d57bdce1aa44b0960e2b7578
maitracle <maitracle@gmail.com>
2025-09-16 02:49:13 +0900

    add start file to discount

---
+++ server/demo/src/main/kotlin/com/example/demo/service/CartWithDiscountService.kt
@@ -0,0 +1,29 @@
+package com.example.demo.service
+
+import com.example.demo.dto.CartItemResponse
+import com.example.demo.dto.CartRequest
+import com.example.demo.dto.CartResponse
+import com.example.demo.repository.ProductRepository
+import com.example.demo.repository.DiscountPolicyRepository
+import java.math.BigDecimal
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+class CartWithDiscountService(
+    private val productRepository: ProductRepository,
+    private val discountPolicyRepository: DiscountPolicyRepository
+) {
+
+    fun calculateCartTotal(cartRequest: CartRequest): CartResponse {
+        TODO("Not yet implemented")
+    }
+
+    fun validateCartItems(cartRequest: CartRequest): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    fun calculateCartTotalWithValidation(cartRequest: CartRequest): CartResponse {
+        TODO("Not yet implemented")
+    }
+}
